8a623bfd1d022e027162819145cd02ff9ed41faf
Klekan <olamilekankamal2@gmail.com>
2023-02-19 18:56:39 -0800

    101-quote.c_commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+return (1);
+}
+
